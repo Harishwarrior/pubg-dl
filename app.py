a4c19ec46991f3bdf7838a9d0f49867647575f92
SivaPrakasam7 <66865924+SivaPrakasam7@users.noreply.github.com>
2021-04-01 02:34:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def getlinks():
     soup = BeautifulSoup(page.content, 'html.parser')
     link = soup.find('a', {'class': 'apk-btn'})['href'] 
     print(link)
-    return "<body style='display:flex;background-image:url('https://wallpapercave.com/wp/wp4907009.jpg');background-repeat: no-repeat;background-attachment: fixed;background-size: cover;'><a style='padding:1rem;background:#BDC3C7;text-decoration:none;color:#1C2833;border-radius:0.3rem;box-shadow:0px 0px 10px 0px #BDC3C7;font-size:large;margin:auto;align-self:center;' href='"+link+"'>PUBG Mobile</a></body>"
+    return "<body style='display:flex;background-image:url(\'https://wallpapercave.com/wp/wp4907009.jpg\');background-repeat: no-repeat;background-attachment: fixed;background-size: cover;'><a style='padding:1rem;background:#BDC3C7;text-decoration:none;color:#1C2833;border-radius:0.3rem;box-shadow:0px 0px 10px 0px #BDC3C7;font-size:large;margin:auto;align-self:center;' href='"+link+"'>PUBG Mobile</a></body>"
     
 @app.route('/pubglite')
 def getlinklite():
@@ -24,7 +24,7 @@ def getlinklite():
     soup = BeautifulSoup(page.content, 'html.parser')
     link = soup.find('a', {'class': 'text-hide spr dl-apk'})['href'] 
     print(link)
-    return "<body style='display:flex;background-image:url('https://wallpapercave.com/wp/wp4211445.jpg');background-repeat: no-repeat;background-attachment: fixed;background-size: cover;'><a style='padding:1rem;background:#BDC3C7;text-decoration:none;color:#1C2833;border-radius:0.3rem;box-shadow:0px 0px 10px 0px #BDC3C7;font-size:large;margin:auto;align-self:center;' href='"+link+"'>PUBG lite</a></body>"
+    return "<body style='display:flex;background-image:url(\'https://wallpapercave.com/wp/wp4211445.jpg\');background-repeat: no-repeat;background-attachment: fixed;background-size: cover;'><a style='padding:1rem;background:#BDC3C7;text-decoration:none;color:#1C2833;border-radius:0.3rem;box-shadow:0px 0px 10px 0px #BDC3C7;font-size:large;margin:auto;align-self:center;' href='"+link+"'>PUBG lite</a></body>"
 
 
 if __name__ == '__main__':
