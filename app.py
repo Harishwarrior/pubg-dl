33196ec52bffb1b528ed4263db608ea8f29bc97d
harishwarrior <warriorharish95668@gmail.com>
2021-04-03 20:15:47 +0530

    removed redundant functions

--- app.py
+++ app.py
@@ -4,30 +4,28 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def getlinks(image, url,btclass):
+    img=image
+    URL = url
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    link = soup.find('a', {'class': btclass})['href'] 
+    print(link)
+    return "<body style='display:flex;background-image:url("+img+");background-repeat: no-repeat;background-attachment: fixed;background-size: cover;background-position:center;'><a style='padding:1rem;background:linear-gradient(65deg,#F4D03F,#FCF3CF);text-decoration:none;color:black;border-radius:0.3rem;box-shadow:0px 0px 10px 2px black;font-size:large;margin:auto;align-self:center;text-shadow:2px 2px 5px black;' href='"+link+"'>PUBG Mobile</a></body>"
+
 @app.route('/')
 def hello():
     return "Hello World!"
 
 @app.route('/pubgm')
-def getlinks():
-    img='https://wallpapercave.com/wp/wp4907009.jpg'
-    URL = 'https://www.pubgmobile.com/en-US/home.shtml'
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    link = soup.find('a', {'class': 'apk-btn'})['href'] 
-    print(link)
-    return "<body style='display:flex;background-image:url("+img+");background-repeat: no-repeat;background-attachment: fixed;background-size: cover;background-position:center;'><a style='padding:1rem;background:linear-gradient(65deg,#F4D03F,#FCF3CF);text-decoration:none;color:black;border-radius:0.3rem;box-shadow:0px 0px 10px 2px black;font-size:large;margin:auto;align-self:center;text-shadow:2px 2px 5px black;' href='"+link+"'>PUBG Mobile</a></body>"
-    
+def run():
+    return getlinks("https://wallpapercave.com/wp/wp4907009.jpg","https://www.pubgmobile.com/en-US/home.shtml",'apk-btn')
+
 @app.route('/pubglite')
-def getlinklite():
-    img='https://wallpapercave.com/wp/wp6075241.jpg'
-    URL = 'https://www.pubgmlite.com/en-US/'
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    link = soup.find('a', {'class': 'text-hide spr dl-apk'})['href'] 
-    print(link)
-    return "<body style='display:flex;background-image:url("+img+");background-repeat: no-repeat;background-attachment: fixed;background-size: cover;background-position:center;'><a style='padding:1rem;background:linear-gradient(65deg,#F4D03F,#FCF3CF);text-decoration:none;color:black;border-radius:0.3rem;box-shadow:0px 0px 10px 2px black;font-size:large;margin:auto;align-self:center;text-shadow:2px 2px 5px black;' href='"+link+"'>PUBG lite</a></body>"
+def run():
+    return getlinks('https://wallpapercave.com/wp/wp6075241.jpg','https://www.pubgmlite.com/en-US/','text-hide spr dl-apk')
 
 
 if __name__ == '__main__':
     app.run()
+
