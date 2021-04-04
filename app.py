c86554b8d5659886b382b36bf9a9558a97112c30
harishwarrior <warriorharish95668@gmail.com>
2021-04-04 23:27:28 +0530

    finally fixed it

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def getlinks(image, url,btclass,apk):
     soup = BeautifulSoup(page.content, 'html.parser')
     link = soup.find('a', {'class': btclass})['href'] 
     print(link)
-    return "<body style='display:flex;background-image:url("+image+");background-repeat: no-repeat;background-attachment: fixed;background-size: cover;background-position:center;'><a style='padding:1rem;background:linear-gradient(65deg,#F4D03F,#FCF3CF);text-decoration:none;color:black;border-radius:0.3rem;box-shadow:0px 0px 10px 2px black;font-size:large;margin:auto;align-self:center;text-shadow:2px 2px 5px black;' href='"+link+"'>'"+apk+"'</a></body>"
+    return "<body style='display:flex;background-image:url("+image+");background-repeat: no-repeat;background-attachment: fixed;background-size: cover;background-position:center;'><a style='padding:1rem;background:linear-gradient(65deg,#F4D03F,#FCF3CF);text-decoration:none;color:black;border-radius:0.3rem;box-shadow:0px 0px 10px 2px black;font-size:large;margin:auto;align-self:center;text-shadow:2px 2px 5px black;' href='"+link+"'>"+apk+"</a></body>"
 
 @app.route('/')
 def hello():
